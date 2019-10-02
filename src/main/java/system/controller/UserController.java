ab2284c6fbad433dcd7f378c54c7c5b8c56dab7d
eurusov <eurusov@users.noreply.github.com>
2019-10-02 05:34:56 +0300

    Initial commit.

--- src/main/java/system/controller/UserController.java
+++ src/main/java/system/controller/UserController.java
@@ -1,11 +1,11 @@
 package system.controller;
 
-import org.springframework.web.bind.annotation.RequestMethod;
-import system.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import system.model.User;
 import system.service.UserService;
 
 import java.util.List;
@@ -17,7 +17,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    @GetMapping("/list")
     public @ResponseBody
     List<User> getAllUsers() {
         return userService.getAllUsers();
